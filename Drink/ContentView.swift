0a1c0c280fd815a0764b244a9c1311306c710368
Leesu <tndusg7@gmail.com>
2024-09-11 16:00:03 +0900

    Initial Commit

---
+++ Drink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drink
+//
+//  Created by 박수연 on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drink/DrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkApp.swift
+//  Drink
+//
+//  Created by 박수연 on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
